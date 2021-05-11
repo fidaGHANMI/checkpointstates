167566dd3badd9940d9f25b94bfe8bb6b47bc89a
fidaghanmi <fidaghanmi@gmail.com>
2021-05-11 23:41:27 +0200

    final commit

--- src/App.js
+++ src/App.js
@@ -7,37 +7,39 @@ class App extends React.Component{
 
   constructor(props) {
     super(props);
-    this.state = {Person: { fullName:"fida ghanmi",bio:27, profession: "engineer"}, 
-                          shows: true};
+    this.state = {person: { fullName:"fida ghanmi",bio:27, profession: "engineer"}, 
+                          shows: true,
+                        time:1};
   
                           console.log(this.state.shows)                      }
 
-  handleClick=()=>this.setState({shows: !this.state.shows.value})
-  
+  handleClick=()=>this.setState({shows: !this.state.shows})
 
-
-  render(){
-return ( 
-<div>
-  <div className="App">
-  <button onClick={this.handleClick}> Click Me </button> </div>)/</div>
-
-if ({this.state.shows}===true) {
- 
-    return( 
-    <div>
-      <h1>{this.state.Person.fullName}</h1>
-    <h1>{this.state.Person.bio}</h1>
-    <h1>{this.state.Person.profession}</h1>
-    </div>)
-   else {
-      return(
-        <div>nothing to show</div>)}
+  componentDidMount() {
     
-      
-    }
+    setInterval(() => {
+      this.setState({time:this.state.time+1});
+    }, 1000);
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <button onClick={this.handleClick}> Click Me </button>
+{
+this.state.shows ?(
+<div> 
+  <h1>{this.state.person.fullName}</h1>
+   <h1>{this.state.person.bio}</h1>
+  <h1>{this.state.person.profession}</h1></div> )
+                :(<div>nothing to show </div>)
 }
 
-}
-}
+    
+      <h1>Number of seconds is {this.state.time}</h1>
+   
+  </div>
+)}
+    }
+
 export default App;
